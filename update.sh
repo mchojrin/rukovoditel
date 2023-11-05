0be32b300a1faf963236f2ffcf7497d75c115c84
mchojrin <mauro.chojrin@leewayweb.com>
2023-11-05 21:55:54 +0100

    Feature: new script to make updates after initial installation

---
+++ update.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+TARGET_IMAGE=ruko_prod
+TAR_FILE="$TARGET_IMAGE.tar"
+ID_FILE="~/.ssh/id_rsa_leeway"
+
+echo "Building production image"
+docker build . -t $TARGET_IMAGE --target=prod
+
+echo "Packaging the image for deployment"
+docker save -o $TAR_FILE $TARGET_IMAGE
+echo "File $TAR_FILE ready for deployment"
+
+IP=$(awk ' {print $1} ' /tmp/droplet.txt)
+
+echo "Uploading files"
+scp -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i $ID_FILE $TAR_FILE root@"$IP":~/
+
+ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i $ID_FILE root@"$IP" "echo 'Bringing down services'; \
+  docker-compose down; \
+	echo 'Installing new image $TARGET_IMAGE'; \
+	docker load -i $TAR_FILE; \
+	echo 'Bringing services up'; \
+	docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d ;"
+
+echo "Everything ready. Open up a browser at http://$IP to see updated version"
